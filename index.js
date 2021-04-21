644f7c266bf9bbc912d62edcecf805171c8499d5
Jessie Wong <kimjang.wong1@gmail.com>
2021-04-21 11:47:03 +0000

    input morgan for logging middleware

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var http = require('http'),
     path = require('path'),
-    express = require('express');
+    express = require('express'),
+    morgan = require('morgan');
 
-    var router = express();
-    var server = http.createServer(router);
+var app = express();
+var server = http.createServer(app);
 
-    router.use(express.static(path.resolve(_dirname, 'views')));
-    router.use(express.urlencoded({extended: true}));
-    router.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(morgan('tiny'));
 
+app.use(require('./routes'));
 
-    router.get('/', function(req, res){
-        res.render('index');
-    });
-
-
-    server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
-        var addr = server.address();
-        console.log("Server listen at", addr.address + ":" + addr.port);
-    });
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
+    var addr = server.address();
+    console.log("Server listen at", addr.address + ":" + addr.port);
+});
