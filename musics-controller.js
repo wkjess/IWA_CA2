0d2acbfb99f274d32f9e436cabfd0fbc85dca250
Jessie Wong <kimjang.wong1@gmail.com>
2021-04-21 13:08:52 +0000

    updated changes

--- musics-controller.js
+++ musics-controller.js
@@ -18,3 +18,30 @@ exports.getMusic = function(req,res){
         res.json(music);
     });
 };
+
+exports.getMusic = function(req, res) {
+    Music.findOne({_id: req.params.id}, function (err, music) {
+        if (err) {
+            res.status(400).json(err);
+        }
+        res.json(music);
+    });
+};
+
+exports.updateMusic = function(req, res) {
+    Music.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function (err, music) {
+        if (err) {
+            res.status(400).json(err);
+        }
+        res.json(music);
+    });
+};
+
+exports.deleteMusic = function(req, res) {
+    Music.findByIdAndRemove(req.params.id, function (err, music) {
+        if (err) {
+            res.status(400).json(err);
+        }
+        res.json(music);
+    });
+};
--- routes.js
+++ routes.js
@@ -6,7 +6,10 @@ router.get('/', function(req, res){
     res.render('index');
 });
 
-router.post('/m1/musics', musicCtrl.createMusics);
 router.get('/m1/musics', musicCtrl.getMusics);
+router.get('/m1/musics/:id', musicCtrl.getMusic);
+router.post('/m1/musics', musicCtrl.createMusic);
+router.post('/m1/musics/:id', musicCtrl.updateMusic);
+router.delete('/m1/musics/:id', musicCtrl.deleteMusic);
 
 module.exports = router;
